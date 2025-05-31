1b2e55e6f45f8cf5b409fafde2916ef85072df86
ZdezLaw <qweqwe5.qq@gmail.com>
2025-05-31 20:32:09 +0300

    fourth commit

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,13 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   build: {
+    assetsDir: "assets",
     rollupOptions: {
-      external: [
-        "src/resources/Yahya_Eyad.pdf",
-        "src/resources/PubliFluorNormale.otf",
-      ],
       output: {
         assetFileNames: (assetInfo) => {
           const info = assetInfo.name.split(".");
           const ext = info[info.length - 1];
-          if (
-            assetInfo.name === "Yahya_Eyad.pdf" ||
-            assetInfo.name === "PubliFluorNormale.otf"
-          ) {
+          if (ext === "pdf" || ext === "otf") {
             return `assets/[name][extname]`;
           }
           return `assets/[name]-[hash][extname]`;
