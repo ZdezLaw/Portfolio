fb6927044814c5ff5fa5460fe4c2b007058fcf9a
ZdezLaw <qweqwe5.qq@gmail.com>
2025-05-31 20:28:29 +0300

    third commit

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,25 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      external: [
+        "src/resources/Yahya_Eyad.pdf",
+        "src/resources/PubliFluorNormale.otf",
+      ],
+      output: {
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split(".");
+          const ext = info[info.length - 1];
+          if (
+            assetInfo.name === "Yahya_Eyad.pdf" ||
+            assetInfo.name === "PubliFluorNormale.otf"
+          ) {
+            return `assets/[name][extname]`;
+          }
+          return `assets/[name]-[hash][extname]`;
+        },
+      },
+    },
+  },
 });
